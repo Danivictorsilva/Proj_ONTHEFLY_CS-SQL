6d009d75a75dc1e8976c4852b3e230d35ce36e15
Daniel Victor Santos Silva <100080815+Danivictorsilva@users.noreply.github.com>
2022-09-28 14:48:24 -0300

    Adicionar arquivos de projeto.

---
+++ Proj_ONTHEFLY_CS-SQL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Proj_ONTHEFLY_CS_SQL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
